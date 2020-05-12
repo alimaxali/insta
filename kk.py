d5c82cd242c31c72c45fbf51c1c4593f71e6f4f5
alimaxali <39878027+alimaxali@users.noreply.github.com>
2020-05-12 12:39:23 +0200

    Update kk.py

--- kk.py
+++ kk.py
@@ -93,7 +93,7 @@ def ali():
  system('clear')
  login = 'https://www.instagram.com/'
  log = login + 'accounts/login/ajax/'
- prox = 'y'
+ prox = 'y.txt'
  if not isfile(prox):exit('[!] File Not Found')
  a.headers = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)\Chrome/59.0.3071.115 Safari/537.36'}
  a.headers.update({'Referer':login})
