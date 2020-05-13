4cdb619f0ecab95d3a19743fb10edbd801c69c6d
alimaxali <39878027+alimaxali@users.noreply.github.com>
2020-05-13 02:15:44 +0200

    Update kk.py

--- kk.py
+++ kk.py
@@ -144,7 +144,7 @@ login = 'https://www.instagram.com/'
 '''
 
 t = Thread(target=special).start()
-t = Thread(target=ali).start()
-t = Thread(target=ali).start()
+#t = Thread(target=ali).start()
+#t = Thread(target=ali).start()
 t = Thread(target=ali).start()
 #ali()
