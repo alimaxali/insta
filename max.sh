ea5614abdaac14a2028948654cc741c3843a7963
alimaxali <39878027+alimaxali@users.noreply.github.com>
2020-08-04 13:15:40 +0200

    Add files via upload

---
+++ max.sh
@@ -0,0 +1,19 @@
+g='\033[1;32m'
+p='\033[1;35m'
+cyan='\033[1;36m'
+green='\033[1;32m'
+r='\033[1;31m'
+yellow='\033[1;33m'
+b='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+y='\033[1;33m'
+#ww = nano l.sh
+
+
+echo -e "$p{$y 1 $p}$g chat"
+echo -e "$p{$y 2 $p}$g facebook"
+echo -e "$p{$y 3 $p}$g call"
+echo -e "$p{$y 4 $p}$g attack"
+echo -e "$p{$y 5 $p}$g Handishk"
+
